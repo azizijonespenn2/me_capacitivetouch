392cd52784d6a271328bf94fbc60b49d2acba91f
azizijonespenn2 <61535698+azizijonespenn2@users.noreply.github.com>
2023-06-14 10:41:45 -0400

    Make heart light when touched instead of dark when touched

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showIcon(IconNames.Heart)
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    basic.pause(200)
     basic.clearScreen()
-    music.playTone(349, music.beat(BeatFraction.Whole))
 })
 pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
+music.playTone(494, music.beat(BeatFraction.Double))
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+	
 })
