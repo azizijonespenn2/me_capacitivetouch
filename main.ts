6dac1a9bf7f9f8dcfc8b691ccc2f931a4fa22074
azizijonespenn2 <61535698+azizijonespenn2@users.noreply.github.com>
2023-08-09 21:09:03 -0700

    Changed to while loop for better interactivity

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    basic.showIcon(IconNames.Heart)
-    music.playTone(175, music.beat(BeatFraction.Whole))
-    basic.pause(200)
-    basic.clearScreen()
-})
 pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
-music.playTone(494, music.beat(BeatFraction.Double))
 basic.forever(function () {
-	
+    while (input.logoIsPressed()) {
+        basic.showIcon(IconNames.Heart)
+        music.playTone(175, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
 })
